578919c2c99a622bd9510462e033a3f0b20410fd
TheOarsman <heinzulrichv@gmail.com>
2024-02-07 13:31:50 -0500

    more corrections

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,5 @@
 module.exports = {
-  format_date: (date) => {
-    // Check if date is provided
-    if (!date) {
-      return "Invalid Date";
-    }
-
+  formatDate: (date) => {
     // Format date as MM/DD/YYYY
     return date.toLocaleDateString();
   },
